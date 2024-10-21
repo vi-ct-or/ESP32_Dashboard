393efecdc587ff171dd8f6a38c9a474e5937355b
victorleroch <leroch@etud.insa-toulouse.fr>
2024-10-21 20:18:54 +0200

    wip ota

--- ESP32_Dashboard/src/main.cpp
+++ ESP32_Dashboard/src/main.cpp
@@ -1,18 +1,31 @@
 #include <Arduino.h>
+#include "credentials.h"
+#include "HTTPClient.h"
+#include "WiFi.h"
 
-// put function declarations here:
-int myFunction(int, int);
+/****** NTP settings ******/
+const char *NTP_SERVER = "pool.ntp.org";
+// your time zone (https://remotemonitoringsystems.ca/time-zone-abbreviations.php)
+const char *TZ_INFO = "CET-1CEST-2,M3.5.0/02:00:00,M10.5.0/03:00:00";
+tm timeinfo;
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+void setup()
+{
+  Serial.begin(9600);
+
+  WiFi.begin(wifiSsid, wifiPswd);
+  Serial.print("Connecting to WiFi ..");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print('.');
+    delay(1000);
+  }
+  Serial.println("connected");
+
+  configTzTime(TZ_INFO, NTP_SERVER);
+  getLocalTime(&timeinfo);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop()
+{
 }
